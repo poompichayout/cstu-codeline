a56b9e08bc301cbd9048811ab5a12426906667f5
poompichayout <poompichayout.kon@dome.tu.ac.th>
2021-08-13 00:17:30 +0700

    config

--- server/config/index.js
+++ server/config/index.js
@@ -1,5 +1,5 @@
 const path = require('path')
-require('dotenv').config({ path: '../.env' });
+require('dotenv').config();
 const { NODE_ENV, DB_CONNECTION, DEV_DB, SECRET } = process.env;
 
 let db_connection = NODE_ENV === 'development' ? DEV_DB : DB_CONNECTION;
